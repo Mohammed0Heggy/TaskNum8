d40782b750b283cb4db32abb202490b00f517c9d
Mohammed Heggy <hegy395@gmail.com>
2023-10-06 19:30:19 +0300

    --

---
+++ JS/Main.js
@@ -0,0 +1,18 @@
+let person = {
+  name: "John",
+  age: 30,
+  isStudent: true
+};
+
+console.log(typeof (person.age))
+
+let numbers = [1, 2, 3, 4, 5];
+let fruits = ["apple", "banana", "orange"];
+
+console.log(typeof (true))
+
+function example() {
+  return;
+}
+
+console.log(typeof (example))
